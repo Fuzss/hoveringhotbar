b6bbbff0e42a211db1b451b91cb3ede572eb1d56
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-09-06 13:38:34 +0200

    simplify ChatComponentMixin.java

--- Common/src/main/java/fuzs/hoveringhotbar/common/mixin/client/ChatComponentMixin.java
+++ Common/src/main/java/fuzs/hoveringhotbar/common/mixin/client/ChatComponentMixin.java
@@ -1,34 +1,22 @@
 package fuzs.hoveringhotbar.common.mixin.client;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import fuzs.hoveringhotbar.common.HoveringHotbar;
 import fuzs.hoveringhotbar.common.config.ClientConfig;
 import net.minecraft.client.gui.components.ChatComponent;
-import net.minecraft.util.Mth;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(ChatComponent.class)
 abstract class ChatComponentMixin {
-    @Shadow
-    @Final
-    private static int BOTTOM_MARGIN;
 
-    @ModifyVariable(method = "extractRenderState(Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;IILnet/minecraft/client/gui/components/ChatComponent$DisplayMode;)V",
-                    at = @At("STORE"),
-                    ordinal = 4)
-    private int extractRenderState(int chatBottom, ChatComponent.ChatGraphicsAccess graphics, int screenHeight, int ticks, ChatComponent.DisplayMode displayMode) {
-        int hotbarOffset = HoveringHotbar.CONFIG.get(ClientConfig.class).getHotbarOffset();
-        if (hotbarOffset == 0) {
-            return chatBottom;
-        }
+    @ModifyExpressionValue(method = "extractRenderState(Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;IILnet/minecraft/client/gui/components/ChatComponent$DisplayMode;)V",
+                           at = @At(value = "CONSTANT", args = "intValue=40"),
+                           slice = @Slice(to = @At(value = "INVOKE",
+                                                   target = "Lnet/minecraft/client/Options;chatOpacity()Lnet/minecraft/client/OptionInstance;")))
+    private int extractRenderState(int bottomMargin) {
+        return bottomMargin + HoveringHotbar.CONFIG.get(ClientConfig.class).getHotbarOffset();
 
-        float scale = (float) this.getScale();
-        return Mth.floor((float) (screenHeight - BOTTOM_MARGIN - hotbarOffset) / scale);
     }
-
-    @Shadow
-    protected abstract double getScale();
 }
